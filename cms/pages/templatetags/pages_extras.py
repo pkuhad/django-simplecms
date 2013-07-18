558ab4eb5bb15769795cb47569eb307bb1b2d84f
Paras Kuhad <paras.kuhad@gmail.com>
2013-07-18 17:15:22 +0530

    Recent page block was showing all pages, fixed now

--- cms/pages/templatetags/pages_extras.py
+++ cms/pages/templatetags/pages_extras.py
@@ -6,6 +6,6 @@ register = template.Library()
 
 @register.inclusion_tag('pages/page-block.html')
 def page_block():
-    pages = Page.objects.all()
+    pages = Page.objects.all()[:5]
     return {'pages':pages}
 
