c5071245315bc611d8cb5eabfe6e2925ba7e6b7e
Paras Kuhad <paras.kuhad@changer.nl>
2013-08-06 11:44:29 +0800

    Generic views with mixins FTW

--- cms/apiv1/views.py
+++ cms/apiv1/views.py
@@ -1,52 +1,17 @@
-from django.http import HttpResponse, Http404
-from django.views.decorators.csrf import csrf_exempt
-
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import mixins, generics
 
 from cms.pages.models import Page
 from cms.apiv1.serializers import PageSerializer
 
 
-class PageList(APIView):
+class PageList(generics.ListCreateAPIView):
     """List all pages"""
     
-    def get(self, request, format=None):
-        pages = Page.objects.all()
-        serializer = PageSerializer(pages, many=True)
-        return Response(serializer.data)
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
 
-    def post(self, request, format=None):
-    	serializer = PageSerializer(data=request.DATA)
-    	if serializer.is_valid():
-    		serializer.save()
-    		return Response(serializer.data, status=status.HTTP_201_CREATED)
-    	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PageDetail(generics.RetrieveUpdateDestroyAPIView):
 
-
-class PageDetail(APIView):
-	def get_object(self, pk):
-		try:
-			return Page.objects.get(pk=pk)
-		except Page.DoesNotExist:
-			raise Http404
-
-	def get(self, request, pk, format=None):
-		page = self.get_object(pk)
-		serializer = PageSerializer(page)
-		return Response(serializer.data)
-
-	def put(self, request, pk, format=None):
-		page = self.get_object(pk)
-		serializer = PageSerializer(page, data=request.DATA)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		else:
-			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	def delete(self, request, pk, format=None):
-		page = self.get_object(pk)
-		page.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+	queryset = Page.objects.all()
+	serializer_class = PageSerializer
